85a547c6ad325f0149cafc8fc74dee466b48a88b
Yongmin Kim <yongmin86k@gmail.com>
2019-08-16 11:44:07 -0700

    create full-width template for the page has a thumbnail

--- themes/inhabitent/page-about.php
+++ themes/inhabitent/page-about.php
@@ -3,9 +3,24 @@
  * The template for displaying all pages.
  *
  * @package RED_Starter_Theme
+ * Template Name: Full-width
  */
 
-get_header(); ?>
+get_header('full-width'); ?>
+	<?php if ( have_posts() && has_post_thumbnail() ): ?>
+		<div class="full-width-image" style="
+			width: 100vw; height: 100vh;
+			background :
+				linear-gradient( to bottom, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.4) 100% ), 
+				url(<?php echo get_the_post_thumbnail_url();?>);
+			background-size: cover;
+			background-position: center; 
+			background-attachment: fixed;
+			background-repeat: no-repeat;
+		">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</div>
+	<?php endif ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
