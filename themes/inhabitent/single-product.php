8baa9f998f61c2d0cbf07f768dd0c82cf1b9c544
Yongmin Kim <yongmin86k@gmail.com>
2019-08-19 15:17:44 -0700

    include social buttons as a template and add styles in single product page

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -18,40 +18,22 @@ get_header(); ?>
 						<?php the_post_thumbnail( 'large' ); ?>
 					<?php endif; ?>
 
-					<?php the_title( '<h1 class="entry-title">', '</h1>' );  ?>
-
-					<div class="product-price">
-						<?php echo CFS()->get( 'product_price' ); ?>
-					</div>
-
-					<div class="entry-meta">
-						<?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
-					</div><!-- .entry-meta -->
 				</header><!-- .entry-header -->
 
 				<div class="entry-content">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' );  ?>
+					<div class="product-price">
+						$<?php echo CFS()->get( 'product_price' ); ?>
+					</div>
 					<?php the_content(); ?>
-					<?php
-						wp_link_pages( array(
-							'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-							'after'  => '</div>',
-						) );
-					?>
+					<div class="wrap-buttons">
+						<?php get_template_part( 'template-parts/content', 'buttons' ); ?>
+					</div>
 				</div><!-- .entry-content -->
 
-				<footer class="entry-footer">
-					<?php red_starter_entry_footer(); ?>
-				</footer><!-- .entry-footer -->
+				
 			</article><!-- #post-## -->
 
-			<?php the_post_navigation(); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
 
 		<?php endwhile; // End of the loop. ?>
 
---
+++ themes/inhabitent/template-parts/content-buttons.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template part for displaying buttons in pages or posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+<div class="social-buttons">
+   <button type="button" class="black-btn"><i class="fab fa-facebook-f"></i>Like</button>
+   <button type="button" class="black-btn"><i class="fab fa-twitter"></i>Tweet</button>
+   <button type="button" class="black-btn"><i class="fab fa-pinterest"></i>Pin</button>
+</div>
