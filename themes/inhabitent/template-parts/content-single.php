94e955d69012f8b9206a0babac81ad98bf6369ed
Yongmin Kim <yongmin86k@gmail.com>
2019-08-20 00:07:25 -0700

    add styles for the single page

--- themes/inhabitent/template-parts/content-single.php
+++ themes/inhabitent/template-parts/content-single.php
@@ -10,7 +10,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
+			<?php the_post_thumbnail( 'full' ); ?>
 		<?php endif; ?>
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
@@ -33,4 +33,7 @@
 	<footer class="entry-footer">
 		<?php red_starter_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
+	<div class="wrap-buttons">
+		<?php get_template_part( 'template-parts/content', 'buttons' ); ?>
+	</div>
 </article><!-- #post-## -->
